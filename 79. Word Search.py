bb8da19f09146de57912a35f7c84c3eda874255c
Naveen Kumar Meena <2022pcecrnaveen038@poornima.org>
2025-04-09 20:54:29 +0530

    Update 79. Word Search.py

--- 79. Word Search.py
+++ 79. Word Search.py
@@ -1,32 +1,39 @@
 class Solution:
-    def exist(self, board, word):
-        if not board or not board[0]:
-            return False
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        counter = Counter([board[x][y] for x in range(ROWS) for y in range(COLS)])
 
-        rows, cols = len(board), len(board[0])
+        counter_word = Counter(word)
 
-        def backtrack(r, c, index):
-            if index == len(word):
+        for char in counter_word:
+            if counter.get(char, 0) < counter_word[char]:
+                return False
+        
+        if counter[word[0]] > counter[word[-1]]:
+            word = word[::-1]
+        
+        def dfs(r, c, i):
+            if i == len(word):
                 return True
-            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != word[index]:
+            
+            if (min(r, c) < 0 or
+                r >= ROWS or c >= COLS or
+                word[i] != board[r][c] or
+                board[r][c] == '#'):
                 return False
             
-            temp = board[r][c]
-            board[r][c] = '#'  # Mark the cell as visited
-
-            found = (backtrack(r + 1, c, index + 1) or
-                     backtrack(r - 1, c, index + 1) or
-                     backtrack(r, c + 1, index + 1) or
-                     backtrack(r, c - 1, index + 1))
-
-            board[r][c] = temp  # Restore the cell
-
-            return found
-
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == word[0]:  # Start searching from the first letter
-                    if backtrack(r, c, 0):
-                        return True
-
+            board[r][c] = '#'
+            res = (dfs(r + 1, c, i + 1) or
+                   dfs(r - 1, c, i + 1) or
+                   dfs(r, c + 1, i + 1) or
+                   dfs(r, c - 1, i + 1))
+            board[r][c] = word[i]
+            return res
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
+                    return True
         return False
+
+#bute force lol
